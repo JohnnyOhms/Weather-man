4634c96fbe9a86a43478561be3df59a67e5e4e1a
JohnnyOhms <officialjohn662@gmail.com>
2022-06-13 20:27:49 +0100

    started js functionality

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-window.onload = fetch("https://api.openweathermap.org/data/2.5/weather?q=arab&appid=8e34c709713c4054f1b1e896136b8571")
-.then((Response)=> Response.json())
-.then((data)=>{
-    console.log(data);
+const api = {
+    base:"https://api.openweathermap.org/data/2.5/weather?q=arab&appid=",
+    key: "8e34c709713c4054f1b1e896136b8571"
+}
+
+const searchBtn = document.querySelector(".head");
+const searchField = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const nameIcon = document.querySelector(".name");
+
+searchBtn.addEventListener('click', (e)=>{
+    nameIcon.classList.add('hide');
+    searchBtn.classList.add('hide');
+    searchField.classList.remove('hide');
+    btn.classList.remove('hide');
+
 })
