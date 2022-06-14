b93353d4426349aa9a09948cccac5eea64af7a98
JohnnyOhms <officialjohn662@gmail.com>
2022-06-14 21:00:15 +0100

    working on fetch api

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 const api = {
-    base:"https://api.openweathermap.org/data/2.5/weather?q=arab&appid=",
-    key: "8e34c709713c4054f1b1e896136b8571"
+    base:"https://api.openweathermap.org/data/2.5/weather?q=",
+    key: "&appid=8e34c709713c4054f1b1e896136b8571"
 }
 
 const searchBtn = document.querySelector(".head");
-const searchField = document.querySelector(".search");
+const input = document.querySelector(".search");
 const btn = document.querySelector(".btn");
 const nameIcon = document.querySelector(".name");
 
 searchBtn.addEventListener('click', (e)=>{
     nameIcon.classList.add('hide');
     searchBtn.classList.add('hide');
-    searchField.classList.remove('hide');
-    btn.classList.remove('hide');
-
+    input.classList.add('slide-down');
+    btn.classList.add('slide-from-left');
+    setTimeout(()=>{
+        input.classList.remove('hide');
+        btn.classList.remove('hide');
+    }, 800)
 })
+
+btn.addEventListener("click",getData)
+
+function getData(e){
+    e.preventDefault();
+    if(input.value == ""){
+        return;
+    }else{
+        let fetchData = fetch(`${api.base}${input.value.toLowerCase()}${api.key}`)   
+        .then(Response=> Response.json())
+        .then(displayData)
+        .catch(err=>{
+         console.error('error')
+        })
+    }
+}
+
+function displayData(data){
+    if(data.cod ==  "404"){ 
+        console.log(data.message);
+        return;
+    }
+    console.log(data);   
+}
